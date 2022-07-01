d079a9febf753e81650c6ad0d2fef0e208a55c2a
C0153 <C0153@8A12.oic.jp>
2022-07-01 12:14:57 +0900

    授業分終了

--- src/C0153.java
+++ src/C0153.java
@@ -1,10 +1,16 @@
 
-public class C0153 extends Student{
+public class C0153 extends Student implements DTO{
 	
 	public C0153(final String name) {
 		
 		super(name);
 		
+	}
+	
+	public int getData() {
+		DTO dto = new StubDTO();
+		
+		return dto.getData();
 	}
 	
 }
